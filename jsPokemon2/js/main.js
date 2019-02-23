4bfeb818697966c36be66070e369766b07c71762
whale <hiwhale3707@naver.com>
2019-02-23 22:57:51 +0900

    updates

--- jsPokemon2/js/main.js
+++ jsPokemon2/js/main.js
@@ -63,7 +63,7 @@ function randSelect(){
 }
 
 mybutton1.addEventListener('click', randSelect);
-mybutton2.addEventListener('click', err);
+mybutton2.addEventListener('click', writeFile);
 
 
 // function readText(that){
@@ -98,4 +98,50 @@ function tableCreate() {
     }
     tbl.appendChild(tbdy);
   }
-  tableCreate();
+tableCreate();
+
+
+//('./test.txt','hi')
+function writeFile(){
+    name ='./test.txt';
+    msg = 'hi';
+	if(name == "") return false;
+
+	var fileObject = new ActiveXObject("Scripting.FileSystemObject");
+
+	// var fullpath = defaultpath+"\\"+name;
+	var fullpath = name;
+
+ // 파일이 생성되어있지 않으면 새로 만들고 기록
+
+	if(!fileObject.FileExists(fullpath)){
+
+		var fWrite = fileObject.CreateTextFile(fullpath,false);
+
+		fWrite.write(msg);
+
+		fWrite.close();
+
+	}else{
+
+// 파일이 이미 생성되어 있으면 appending 모드로 파일 열고 기록
+
+		var fWrite = fileObject.OpenTextFile(fullpath, 8);
+
+		fWrite.write(msg);
+
+		fWrite.close();
+
+	}
+
+}
+
+var database = firebase.database();
+
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
